a0893e60e62de0abd15c506cc95fda5537cdf2a2
thhvvv <makausilvia964@gmail.com>
2024-02-20 01:16:18 -0800

    bubble0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers using bubble sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
